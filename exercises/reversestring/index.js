3ea33d3154c8ab83db685434485800d3dcea07bb
camposss <ccamposs23@gmail.com>
2018-03-10 16:55:56 -0800

    Went over the string reversal problem.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,25 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    // return str.split('').reverse().join('');
+
+    // this only works if you want to iterate in order
+    //if you want to go for every 3rd or 5th for example you would need to use a regular for loop.
+    // let reversed = '';
+    // for (let character of str){
+    //     reversed = character+reversed;
+    // }
+    // return reversed;
+
+    //Using an array helper
+
+    return str.split('').reduce((reversed, character)=>{
+        return character+reversed;
+    }, '');
+
+
+}
+reverse('asdf');
 
 module.exports = reverse;
