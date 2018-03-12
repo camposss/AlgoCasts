85450c709c14841fe96c88ce55824073cb244ff8
camposss <ccamposs23@gmail.com>
2018-03-12 12:13:51 -0700

    Finished the reverse an int problem.

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,22 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    //my version of the problem
+    // if(n===0){
+    //     return 0;
+    // }
+    // const toString= n.toString();
+    // let reversed = toString.split('').reverse().join('');
+    // if(Math.sign(n)!==1){
+    //     const addNegative = "-"+ reversed
+    //     return parseInt(addNegative);
+    // }
+    // return parseInt(reversed);
+    
+    const reversed= n.toString().split('').reverse().join('');
+    return parseInt(reversed) *Math.sign(n);
+
+}
 
 module.exports = reverseInt;
