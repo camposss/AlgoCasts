c0092c45caf20831e85f1963938e7a4fbbdc830e
camposss <ccamposs23@gmail.com>
2018-03-20 09:28:59 -0700

    Completed the vowels practice problem using regex and iterative approach.

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,48 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+//using regex///
+function vowels(str) {
+    //g means to keep going after first match
+    //i means case insensitive
+    //returns an array of matches or will return null
+   const matches = str.match(/[aeiou]/gi);
+   return matches ? matches.length: 0;
+}
 
+//first iterative solution///
+// function vowels(str) {
+//     let vowels =0;
+//     const checker = ['a','e','i','o','u'];
+//     for(let char of str.toLowerCase()){
+//        if(checker.includes(char)){
+//            vowels++;
+//        }
+//     }
+//     return vowels;
+// }
+
+
+////My function using a switch statement
+
+// function vowels(str) {
+//     let vowels =0;
+//     for(letter of str){
+//         switch(letter){
+//             case 'a':
+//             case 'A':
+//             case 'e':
+//             case 'E':
+//             case 'i':
+//             case 'I':
+//             case 'o':
+//             case 'O':
+//             case 'u':
+//             case 'U':
+//                 vowels++;
+//                 break;
+//         }
+//     }
+//     return vowels;
+// }
 module.exports = vowels;
