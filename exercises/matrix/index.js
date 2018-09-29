52a859d82ebbf1d5537282c688b11dd326e39edc
Christian Campos <christian.campos@compositeapps.net>
2018-09-28 21:25:56 -0700

    went over matrix problem.

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,46 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let results = [];
+    for ( let i = 0; i <n; i++){
+        results.push([]);
+    }
+    let counter = 1;
+    let start_col = 0;
+    let end_col = n-1;
+    let start_row = 0;
+    let end_row = n-1;
+
+    while(start_col <= end_col && start_row<= end_row){
+        //  top row
+        for (let i = start_col; i <= end_col; i++){
+            results[start_row][i]= counter;
+            counter++;
+        }
+        start_row++;
+        
+        // Right Column
+        for (let i =start_row; i<= end_row; i++){
+            results[i][end_col] = counter; 
+            counter++;
+        }
+        end_col--;
+        // Bottom row
+        for ( let i = end_col; i >= start_col; i--){
+            results[end_row][i] = counter;
+            counter++;
+        }
+        end_row--;
+
+        // start column
+        for(let i = end_row; i>= start_row; i--){
+            results[i][start_col] = counter;
+            counter++;
+        }
+        start_col++;
+    }
+    return results;
+}
 
 module.exports = matrix;
