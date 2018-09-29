5be817faecca4b05ae23d5057f29c7f4f8803fa8
Christian Campos <christian.campos@compositeapps.net>
2018-09-29 12:40:40 -0700

    worked on fib problem

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,42 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// Recursive Solution
+// Exponential Time Soltion: O(2^n) --> BAD
+
+function memoize(func){
+    const cache = {};
+    // idk how many args this will receive
+    return function(...args){
+        if(cache[args]){
+            return cache[args];
+        }
+        const result = func.apply(this, args);
+        cachce[args]= result;
+        return result;
+    }
+}
+function fib(n) {
+
+    if(n< 2){
+        return n;
+    }
+    return fib(n-1) + fib(n-2);
+    
+}
+fib = memoize(slowFib);
+
+// Iterative Solution
+// O(n)
+// function fib(n) {
+//     // start with array of [0,1]
+//     let results = [0,1];
+//     for(i = 2 ; i <= n; i++  ){
+//         let calcNumber = (results[i-1]+ results[i-2]);
+//         results.push(calcNumber);
+//     }
+//     return results[results.length-1];
+// }
+
 
 module.exports = fib;
