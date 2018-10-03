6b3c85cb1a163f899064d9d70ac3a94da4f55005
camposss <ccamposs23@gmail.com>
2018-10-03 07:49:57 -0700

    Finished up the course, must go back and review

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,40 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right= null;
+    }
+    insert(data){
+        const node = new Node(data);
+        // using recursion
+        // checking the left side
+        if(data < this.data && this.left){
+            this.left.insert(data);
+        }else if(data < this.data){
+            this.left = new Node(data);
+            // check the right side
+        }else if(data > this.data && this.right){
+            this.right.insert(data);
+        }else if(data > this.data){
+            this.right = new Node(data);
+        }
+    }
+    contains(data){
+        if(this.data === data){
+            return this;
+        }
+        // If incoming data is greater than we must go to the right
+        if(this.data < data && this.right ){
+            return this.right.contains(data);
+            // If incoming data is less than we must go to the left
+        }else if(this.data > data && this.left){
+            return this.left.contains(data);
+        }
+        return null;
+    }
+}
 
 module.exports = Node;
--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,20 +1,63 @@
 // --- Directions
 // Implement bubbleSort, selectionSort, and mergeSort
 
+// Usually people don't get asked as much for interviews
+// Focus on this LAST
 function bubbleSort(arr) {
-
+    // O(n^2)
+    for(let i = 0 ; i< arr.length; i++){
+        for(let j = 0; j < (arr.length-i-1); j++){
+            if(arr[j] > arr[j+1]){
+                const lesser = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j]= lesser;
+            }
+        }
+    }
 }
 
 function selectionSort(arr) {
-
+    // O(n^2)
+    for(let i = 0 ; i<arr.length; i++){
+        let indexOfMin = i;
+        // Prove me WRONG!
+        for (let j = i+1; j < arr.length; j++){
+            if(arr[j] < arr[indexOfMin]){
+                indexOfMin = j;
+            }
+        }
+        if(indexOfMin !== i){
+            let lesser = arr[indexOfMin];
+            arr[indexOfMin] = arr[i];
+            arr[i] = lesser;
+        }
+    }
 }
 
 function mergeSort(arr) {
-
+    // n*log(n)
+    if(arr.length ===1){
+        return arr;
+    }
+    const center = Math.floor(arr.length /2);
+    // [0, center), doesn't include center
+    const left = arr.slice(0,center);
+    // everything after center inclusive
+    const right = arr.slice(center);
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
-
+    const results = [];
+    while(left.length && right.length){
+        if(left[0] < right[0]){
+            results.push(left.shift());
+        }else {
+            results.push(right.shift());
+        }
+    }
+    // same as calling concat function
+    return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort };
